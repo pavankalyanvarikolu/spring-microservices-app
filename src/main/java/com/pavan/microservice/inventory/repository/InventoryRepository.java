3c9c73ae9b5086762b19ab64947666c441199a50
pavan kalyan <p.kalyanvarikolu@gmail.com>
2024-09-06 20:33:04 +0100

    adding all the files of inventory-service

---
+++ src/main/java/com/pavan/microservice/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.pavan.microservice.inventory.repository;
+
+import com.pavan.microservice.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, int quantity);
+}
